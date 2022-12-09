7011a00f1ad2d96a4224dabcb298fe7b2cf1c872
AndreaMinelli <minelliandrea94@gmail.com>
2022-12-09 13:10:48 +0100

    Targhettizzati elementi in pagina

--- js/script.js
+++ js/script.js
@@ -7,4 +7,9 @@ L'utente, tramite 3 input, deve avere la possibilità di definire:
 Cliccando sul tasto "genera", vengono generati i box con i parametri specificati dall'utente e andremo a colorare di VERDE i numeri pari e di ROSSO i numeri dispari.
 */
 
-//
+//Targhettizzo gli elementi in pagina
+
+const userBoxNumber = document.getElementById('box-number');
+const userMin = document.getElementById('min-number');
+const userMax = document.getElementById('max-number');
+const generate = document.getElementById('generate');
