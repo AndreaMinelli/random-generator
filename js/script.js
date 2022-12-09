d3192c0f71327e3cea22ab9bb1fbb7820a8b3bba
AndreaMinelli <minelliandrea94@gmail.com>
2022-12-09 12:57:11 +0100

    Impostata struttura form

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log('JS OK')
+/*
+Scrivi un applicazione che sia in grado di generare una serie di numeri randomici.
+L'utente, tramite 3 input, deve avere la possibilità di definire:
+- Quanti box generare
+- il numero minimo di generazione dei numeri random
+- il numero massimo di generazione dei numeri random
+Cliccando sul tasto "genera", vengono generati i box con i parametri specificati dall'utente e andremo a colorare di VERDE i numeri pari e di ROSSO i numeri dispari.
+*/
+
+//
